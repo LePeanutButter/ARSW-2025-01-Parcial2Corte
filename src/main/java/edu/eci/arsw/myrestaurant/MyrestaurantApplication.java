44925bf67c32e94ef5670dbbc1a1acd779b48572
LePeanutButter <santi6botero@gmail.com>
2025-10-31 09:18:55 -0500

    fix: Fixed pom.xml to successfully compile the project

--- src/main/java/edu/eci/arsw/myrestaurant/MyrestaurantApplication.java
+++ src/main/java/edu/eci/arsw/myrestaurant/MyrestaurantApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestapidemoApplication {
+public class MyrestaurantApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestapidemoApplication.class, args);
+		SpringApplication.run(MyrestaurantApplication.class, args);
 	}
+
 }
